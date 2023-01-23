269805e06a4409932387897a04eaa98e4532e52d
son1122 <wongsatorn.n@hotmail.com>
2023-01-23 20:51:41 +0700

    format

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -2,9 +2,9 @@ import "./About.css"
 
 const About = () => {
     return (
-      <div className="about-container">
-        <h1>This Is About me</h1>
-      </div>
+        <div className="about-container">
+            <h1>This Is About me</h1>
+        </div>
     );
 }
 
